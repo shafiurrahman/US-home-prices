e70599ca11930b52a8d8fcc3fc1ea81906d5563c
shafiurrahman <surnorth2003@gmail.com>
2025-08-12 14:13:38 +0000

    12aug

---
+++ Flask/flask/jinja.py
@@ -0,0 +1,54 @@
+#### Building Url Dynamically
+## Variable Rule
+### Jinja 2 Template Engine
+
+from flask import Flask,render_template,request,redirect,url_for
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+###WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Welcome to the flask course</H1></html>"
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'hello {name} !'
+    return render_template('form.html')
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+#variable rule : ie. in our case we can give only int for score, if float is give there i/p has to be like 65.0
+@app.route('/success/<int:score>')    # works with string  if return is 'your score'+score becasue of concat
+def success(score):
+    res=''
+    if score >=50 and score <=100:
+        res='********passed'
+    elif score >100:
+        res='?? enter correct number'
+    else:
+        res='failed ^^^^^^'
+    return render_template('result.html',result=res)  # this {{result}} is jinja2 template accessed in result.html
+    
+
+
+if __name__==  "__main__":    # this is the entry point of all .pyfiles
+    app.run(debug=True)   #debug=true will restart the server i.e changes made here will reflect on webpage
+
